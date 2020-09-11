df2a3634e3e7d0ea549f24ffa7033367b3254d63
CarlaAll <69358793+CarlaAll@users.noreply.github.com>
2020-09-11 14:53:32 +0200

    adding ifcondition for classes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,10 +2,36 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import "./BackgroundColor.css";
 
 export default function WeatherInfo(props) {
+  let temp = Math.round(props.temp);
   return (
-    <div className="WeatherInfo">
+    <div
+      className={`${
+        temp > -30 && temp <= -15
+          ? `cold-3`
+          : temp > -15 && temp < -7.5
+          ? `cold-2`
+          : temp >= -7.5 && temp <= 0
+          ? `cold-1`
+          : temp > 0 && temp <= 5
+          ? `cold`
+          : temp > 5 && temp <= 8
+          ? `warm-1`
+          : temp > 8 && temp <= 11
+          ? `warm-2`
+          : temp > 11 && temp <= 15
+          ? `warm-3`
+          : temp > 15 && temp <= 20
+          ? `warm-4`
+          : temp > 20 && temp <= 25
+          ? `warm-5`
+          : temp > 25 && temp <= 30
+          ? `warm-6`
+          : `warm-default`
+      }`}
+    >
       <h1>
         {props.data.city}, {""}
         {props.data.country}
